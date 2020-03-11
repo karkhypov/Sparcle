f9334bbd0f92172ae78f4397fd5f7246ee723eb6
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-03-11 18:56:34 +0200

    implemented booking form functionality, refactoring required

---
+++ src/scripts/booking-prices.js
@@ -0,0 +1,52 @@
+const prices = {
+  residential: {
+    bedroom: {
+      price: 8,
+      time: 1,
+    },
+    bathroom: {
+      price: 10,
+      time: 1,
+    },
+  },
+  commercial: {
+    'Business Offices': {
+      price: 5,
+      time: 1,
+    },
+    Government: {
+      price: 4,
+      time: 1,
+    },
+    'Medical & Healthcare': {
+      price: 7,
+      time: 1,
+    },
+    'Fitness Centers': {
+      price: 6,
+      time: 1,
+    },
+    'Schools and Universities': {
+      price: 4,
+      time: 1,
+    },
+    'Commercial Kitchens': {
+      price: 8,
+      time: 1,
+    },
+    'Construction Clean Up': {
+      price: 13,
+      time: 1,
+    },
+    'Industrial & Manufacturing': {
+      price: 11,
+      time: 1,
+    },
+    'Religious Facilities': {
+      price: 4,
+      time: 1,
+    },
+  },
+};
+
+export default prices;
--- src/scripts/booking.js
+++ src/scripts/booking.js
@@ -1,39 +1,170 @@
+import prices from './booking-prices';
+
 const residential = document.querySelector('#residential');
 const commercial = document.querySelector('#commercial');
-const residentialBtn = document.querySelector('#residentialBtn');
-const commercialBtn = document.querySelector('#commercialBtn');
+const residentialButton = document.querySelector('#residentialButton');
+const commercialButton = document.querySelector('#commercialButton');
 const type = document.querySelector('.booking-type');
 
-const subtotalPrice = document.querySelector('.booking-subtotal span');
+const options = document.querySelector('.booking-options');
+const bedrooms = document.querySelector('#bedrooms');
+const bathrooms = document.querySelector('#bathrooms');
+const facility = document.querySelector('.booking-option__select');
+const area = document.querySelector('.booking-option__input');
+
+const subtotalDescription = document.querySelector('.booking-subtotal__description');
+const subtotalPrice = document.querySelector('.booking-subtotal__price');
+const subtotalHours = document.querySelector('.booking-subtotal__hours');
 const subtotalType = document.querySelector('.booking-subtotal__type');
-const subtotalValues = document.querySelectorAll('.booking-subtotal__value');
+
+const form = document.querySelector('.booking-form');
+const fullName = document.querySelector('input[name="fullName"]');
+const phone = document.querySelector('input[name="phone"]');
+const email = document.querySelector('input[name="email"]');
+
+function calculateSubtotal(facilityType) {
+  if (facilityType) {
+    subtotalPrice.textContent = (prices.commercial[facilityType].price || 100) * (area.value || 100);
+    subtotalHours.textContent = `${Math.ceil((prices.commercial[facilityType].hours || 1) * ((area.value || 100) / 50))} hours`;
+    return;
+  }
+  subtotalPrice.textContent =
+    +bedrooms.dataset.value * prices.residential.bedroom.price + +bathrooms.dataset.value * prices.residential.bathroom.price;
+  subtotalHours.textContent = `${+bedrooms.dataset.value * prices.residential.bedroom.time +
+    +bathrooms.dataset.value * prices.residential.bathroom.time} hours`;
+}
+
+calculateSubtotal();
+
+options.addEventListener('click', e => {
+  if (e.target.closest('.booking-bedrooms__plus')) {
+    bedrooms.dataset.value = +bedrooms.dataset.value + 1;
+    bedrooms.dataset.value = bedrooms.dataset.value > 10 ? 10 : bedrooms.dataset.value;
+    bedrooms.textContent = bedrooms.dataset.value;
+    subtotalDescription.querySelector('.booking-subtotal__bedrooms').textContent = `${bedrooms.dataset.value} bedroom${
+      bedrooms.dataset.value > 1 ? 's' : ''
+    }`;
+    calculateSubtotal();
+  }
+
+  if (e.target.closest('.booking-bedrooms__minus')) {
+    bedrooms.dataset.value = +bedrooms.dataset.value - 1;
+    bedrooms.dataset.value = bedrooms.dataset.value < 1 ? 1 : bedrooms.dataset.value;
+    bedrooms.textContent = bedrooms.dataset.value;
+    subtotalDescription.querySelector('.booking-subtotal__bedrooms').textContent = `${bedrooms.dataset.value} bedroom${
+      bedrooms.dataset.value > 1 ? 's' : ''
+    }`;
+    calculateSubtotal();
+  }
+
+  if (e.target.closest('.booking-bathrooms__plus')) {
+    bathrooms.dataset.value = +bathrooms.dataset.value + 1;
+    bathrooms.dataset.value = bathrooms.dataset.value > 10 ? 10 : bathrooms.dataset.value;
+    bathrooms.textContent = bathrooms.dataset.value;
+    subtotalDescription.querySelector('.booking-subtotal__bathrooms').textContent = `${bathrooms.dataset.value} bathroom${
+      bathrooms.dataset.value > 1 ? 's' : ''
+    }`;
+    calculateSubtotal();
+  }
+
+  if (e.target.closest('.booking-bathrooms__minus')) {
+    bathrooms.dataset.value = +bathrooms.dataset.value - 1;
+    bathrooms.dataset.value = bathrooms.dataset.value < 1 ? 1 : bathrooms.dataset.value;
+    bathrooms.textContent = bathrooms.dataset.value;
+    subtotalDescription.querySelector('.booking-subtotal__bathrooms').textContent = `${bathrooms.dataset.value} bathroom${
+      bathrooms.dataset.value > 1 ? 's' : ''
+    }`;
+    calculateSubtotal();
+  }
+});
+
+facility.addEventListener('change', e => {
+  subtotalDescription.querySelector('.booking-subtotal__facility').textContent = e.target.value;
+  calculateSubtotal(facility.value);
+});
+
+area.addEventListener('change', e => {
+  e.target.value = e.target.value < 100 ? 100 : e.target.value;
+  e.target.value = e.target.value > 10000 ? 10000 : e.target.value;
+  subtotalDescription.querySelector('.booking-subtotal__area').textContent = e.target.value;
+  calculateSubtotal(facility.value);
+});
+
+area.addEventListener('input', e => {
+  subtotalDescription.querySelector('.booking-subtotal__area').textContent = e.target.value || 100;
+  calculateSubtotal(facility.value);
+});
 
 type.addEventListener('click', e => {
-  if (e.target.matches('#residentialBtn')) {
-    residentialBtn.dataset.active = true;
-    residentialBtn.className = 'btn1';
+  if (e.target.matches('#residentialButton')) {
+    residentialButton.dataset.active = true;
+    residentialButton.className = 'btn1';
 
-    commercialBtn.className = 'btn2';
-    commercialBtn.dataset.active = false;
+    commercialButton.className = 'btn2';
+    commercialButton.dataset.active = false;
 
     residential.style.display = 'flex';
     commercial.style.display = 'none';
 
     residential.dataset.active = 'true';
     commercial.dataset.active = 'false';
+
+    subtotalType.textContent = 'Residential Cleaning - ';
+    subtotalDescription.innerHTML = `Includes: <span class = "booking-subtotal__bedrooms">${bedrooms.dataset.value} bedroom${
+      bedrooms.dataset.value > 1 ? 's' : ''
+    }</span> <span class = "booking-subtotal__bathrooms">${bathrooms.dataset.value} bathroom${bathrooms.dataset.value > 1 ? 's' : ''}</span>`;
+    calculateSubtotal();
   }
 
-  if (e.target.matches('#commercialBtn')) {
-    residentialBtn.dataset.active = false;
-    residentialBtn.className = 'btn2';
+  if (e.target.matches('#commercialButton')) {
+    residentialButton.dataset.active = false;
+    residentialButton.className = 'btn2';
 
-    commercialBtn.className = 'btn1';
-    commercialBtn.dataset.active = true;
+    commercialButton.className = 'btn1';
+    commercialButton.dataset.active = true;
 
     residential.style.display = 'none';
     commercial.style.display = 'flex';
 
     residential.dataset.active = 'false';
     commercial.dataset.active = 'true';
+
+    subtotalType.textContent = 'Commercial Cleaning - ';
+    subtotalDescription.innerHTML = `Type: <span class = "booking-subtotal__facility">${
+      facility.value
+    }</span> Area: <span class = "booking-subtotal__area">${area.value || 100}</span>m²`;
+    calculateSubtotal(facility.value);
   }
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const description = subtotalDescription.querySelectorAll('span');
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+  const data = {
+    price: subtotalPrice.textContent,
+    time: subtotalHours.textContent.slice(0, -6),
+    type: subtotalType.textContent.slice(0, -3),
+    description: {
+      1: description[0].textContent,
+      2: description[1].textContent,
+    },
+    name: fullName.value,
+    phone: phone.value,
+    email: email.value,
+  };
+  try {
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(response => response.json())
+      .then(json => console.log(json));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+  e.target.reset();
 });
