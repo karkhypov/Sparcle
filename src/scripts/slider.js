2d3769cc62429843b17da8d17104bbb0faee0e1d
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-03-22 17:25:04 +0200

    slider fix

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -2,19 +2,22 @@ const slider = document.querySelector('.slider');
 
 let current = slider.querySelector('.current') || slider.firstElementChild;
 let prev = current.previousElementSibling || slider.lastElementChild;
+let beforePrev = prev.previousElementSibling || slider.lastElementChild;
 let next = current.nextElementSibling || slider.firstElementChild;
 
 function applyClasses() {
   current.classList.add('current');
   prev.classList.add('prev');
+  beforePrev.classList.add('before-prev');
   next.classList.add('next');
 }
 
 function move() {
+  beforePrev.classList.remove('before-prev');
   prev.classList.remove('prev');
   current.classList.remove('current');
   next.classList.remove('next');
-  [prev, current, next] = [current, next, next.nextElementSibling || slider.firstElementChild];
+  [beforePrev, prev, current, next] = [prev, current, next, next.nextElementSibling || slider.firstElementChild];
   applyClasses();
 }
 
